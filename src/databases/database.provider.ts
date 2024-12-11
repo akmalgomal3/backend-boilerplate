a8184e442f3db9b1df308c0746c9710355c61b5a
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-12-11 21:17:19 +0700

    feat: setup database module

--- src/databases/database.provider.ts
+++ src/databases/database.provider.ts
@@ -3,37 +3,35 @@ import { MongoClient } from 'mongodb';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
-    {
-        provide: 'DB_POSTGRES',
-        inject: [ConfigService],
-        useFactory: async (configService: ConfigService) => {
-            const dataSource = new DataSource({
-                type: 'postgres',
-                host: configService.get<string>('DB_HOST'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_NAME'),
-                entities: [
-                    __dirname + '/../**/*.entity{.ts,.js}',
-                ],
-                synchronize: false,
-            });
+  {
+    provide: 'DB_POSTGRES',
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: false,
+      });
 
-            return dataSource.initialize();
-        },
+      return dataSource.initialize();
     },
-    {
-        provide: 'DB_MONGODB',
-        inject: [ConfigService],
-        useFactory: async (configService: ConfigService) => {
-            const client = new MongoClient(configService.get<string>('MONGODB_URL'), {
-                connectTimeoutMS: 10000,
-                socketTimeoutMS: 45000,
-            });
+  },
+  {
+    provide: 'DB_MONGODB',
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const client = new MongoClient(configService.get<string>('MONGODB_URL'), {
+        connectTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+      });
 
-            await client.connect();
-            return client.db(configService.get<string>('MONGODB_DBNAME'));
-        },
+      await client.connect();
+      return client.db(configService.get<string>('MONGODB_DBNAME'));
     },
+  },
 ];
