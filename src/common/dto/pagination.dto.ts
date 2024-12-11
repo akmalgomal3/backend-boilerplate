cc59b1a93f864486f61a5b2c64817146221c588c
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-12-11 21:27:08 +0700

    feat: setup pagination dto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,14 +1,27 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PaginationDto {
-    page: number;
-    limit: number;
+  @ApiProperty({ example: 1, description: 'Page', default: 1 })
+  @IsNotEmpty()
+  @IsNumber()
+  @Type(() => Number)
+  page: number = 1;
+
+  @ApiProperty({ example: 10, description: 'Limit data per page', default: 10 })
+  @IsNotEmpty()
+  @IsNumber()
+  @Type(() => Number)
+  limit: number = 10;
 }
 
 export class PaginatedResponseDto<T> {
-    data: T[];
-    metadata: {
-        page: number;
-        limit: number;
-        totalPages: number;
-        totalItems: number;
-    };
+  data: T[];
+  metadata: {
+    page: number;
+    limit: number;
+    totalPages: number;
+    totalItems: number;
+  };
 }
