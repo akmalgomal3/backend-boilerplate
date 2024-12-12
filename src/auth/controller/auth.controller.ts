adb820af25e4c5dac973118bce9a92037cb67a75
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-12-12 08:53:56 +0700

    feat: add generate password API

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { GeneratePasswordDto } from '../dto/generate-password.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/generate-password')
+  generatePassword(@Body() generatePasswordDto: GeneratePasswordDto) {
+    const data = this.authService.generatePassword(generatePasswordDto);
+
+    return {
+      data,
+    };
+  }
 }
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { UtilsService } from '../../libs/utils/services/utils.service';
+import { GeneratePasswordDto } from '../dto/generate-password.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly utils: UtilsService) {}
+
+  generatePassword(generatePasswordDto: GeneratePasswordDto) {
+    try {
+      const { password, confirmPassword } = generatePasswordDto;
+
+      const encryptedPassword = this.utils.encrypt(password);
+      const encryptedConfirmPassword = this.utils.encrypt(confirmPassword);
+
+      return {
+        password: encryptedPassword,
+        confirmPassword: encryptedConfirmPassword,
+      };
+    } catch (e) {
+      throw new HttpException(
+        e.message || 'Error generating password',
+        e.status || 500,
+      );
+    }
+  }
+}
