f48c1186f4ab33bad0b384555c671c1bf1da8420
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-12-12 08:00:40 +0700

    fix: db postgresql synchronize to false

--- src/databases/database.provider.ts
+++ src/databases/database.provider.ts
@@ -15,7 +15,7 @@ export const databaseProviders = [
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
         logging: true,
       });
 
