a3c1f45ee18b436b82b3669b3e7aedd034afc12f
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-12-23 16:13:15 +0700

    fix: email template

--- src/libs/email/templates/email.templates.ts
+++ src/libs/email/templates/email.templates.ts
@@ -4,6 +4,8 @@ export const verificationEmailTemplate = (name: string, token: string) => {
       <h1>Hello, ${name}</h1>
       <p>Thank you for registering on our platform. To complete your registration, please click the link below:</p>
       <a href="${process.env.SERVER_BASE_URL}/auth/verify-email?token=${token}">Verify email</a>
+      <p>If you cant click the link, please go to </p>
+      <p>${process.env.SERVER_BASE_URL}/auth/verify-email?token=${token}</p>
     </div>
   `;
 };
@@ -17,6 +19,8 @@ export const sendForgotPasswordEmailTemplate = (
       <h1>Hello, ${name}</h1>
       <p>We received a request to reset your password. To reset your password, please click the link below:</p>
       <a href="${process.env.SERVER_BASE_URL}/auth/reset-password?token=${token}">Reset password</a>
+      <p>If you cant click the link, please go to </p>
+      <p>${process.env.SERVER_BASE_URL}/auth/reset-password?token=${token}</p>
     </div>
   `;
 };
@@ -27,6 +31,8 @@ export const sendUpdateEmailTemplate = (name: string, token: string) => {
       <h1>Hello, ${name}</h1>
       <p>We received a request to update your email. To verify your email, please click link below:</p>
       <a href="${process.env.SERVER_BASE_URL}/auth/update-email?token=${token}">Update email</a>
+      <p>If you cant click the link, please go to </p>
+      <p>${process.env.SERVER_BASE_URL}/auth/update-email?token=${token}</p>
     </div>
   `;
 };
