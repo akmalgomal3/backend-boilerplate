e5a96049a674823f8f017b09de579090a92b0c52
Muhamad Akmal <muhamad.akmal01@ui.ac.id>
2025-01-09 16:34:34 +0700

    feat: Add new endpoint to bulk update and bulk delete

--- src/roles/repository/roles.repository.ts
+++ src/roles/repository/roles.repository.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { DataSource, ILike, Repository } from 'typeorm';
+import { DataSource, ILike, In, Repository } from 'typeorm';
 import { CreateRoleDto } from '../dto/create-roles.dto';
 import { UpdateRoleDto } from '../dto/update-roles.dto';
 import { Roles } from '../entity/roles.entity';
@@ -133,6 +133,23 @@ export class RolesRepository {
     }
   }
 
+  async bulkDeleteRoles(roleIds: string[]): Promise<void> {
+    const queryRunner = this.repository.manager.connection.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.delete(Roles, { roleId: In(roleIds) });
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
   async getBaseRole(): Promise<Roles | null> {
     try {
       const role = await this.repository.findOne({
@@ -145,6 +162,36 @@ export class RolesRepository {
       return role || null;
     } catch (error) {
       throw error;
+    }
+  }
+
+  async bulkUpdateRoles(
+    updates: { roleId: string; updateRoleDto: UpdateRoleDto }[],
+    userId: string,
+  ): Promise<void> {
+    const queryRunner = this.repository.manager.connection.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      for (const { roleId, updateRoleDto } of updates) {
+        const updateData = {
+          roleName: updateRoleDto.roleName || undefined,
+          roleType: updateRoleDto.roleType || undefined,
+          updatedBy: userId,
+          updatedAt: new Date(),
+        };
+
+        await queryRunner.manager.update(Roles, { roleId }, updateData);
+      }
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
   }
 }
